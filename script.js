92501582b309648d00cc3e553c8f4047499fbc80
msedkiewicz <magdalenainspirations@gmail.com>
2023-07-11 20:31:10 +0200

    Add questions; change colors; code formatting

--- script.js
+++ script.js
@@ -1,54 +1,190 @@
 // Questions will be asked
-const Questions = [{
-  id: 0,
-  q: "1. Wydaje mi się, że docierają do mnie różne subtelne szczegóły otoczenia.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-},
-{
-  id: 1,
-  q: "2. Podlegam wpływowi nastrojów innych ludzi.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-},
-{
-  id: 2,
-  q: "3. Mam skłonność do dużej wrażliwości na ból.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-},
-{
-  id: 3,
-  q: "4. Jestem bardzo czuła na działanie kofeiny.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-},
-{
-  id: 4,
-  q: "5. W dni, kiedy dużo się dzieje, miewam potrzebę znalezienia chwili dla siebie, chowając się do łóżka, zaciemnionego pokoju czy gdziekolwiek indziej, gdzie mogę zaznać trochę spokoju i uciec od bodźców.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-},
-{
-  id: 5,
-  q: "6. Łatwo przytłaczają mnie takie bodźce jak ostre światło, silne zapachy, szorstkie tkaniny czy syreny przejeżdżających karetek.",
-  a: [{ text: "tak", isCorrect: true },
-  { text: "nie", isCorrect: false },
-  ]
-
-}
-
-]
+const Questions = [
+  {
+    id: 0,
+    q: "1. Wydaje mi się, że docierają do mnie różne subtelne szczegóły otoczenia.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 1,
+    q: "2. Podlegam wpływowi nastrojów innych ludzi.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    q: "3. Mam skłonność do dużej wrażliwości na ból.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    q: "4. W dni, kiedy dużo się dzieje, miewam potrzebę znalezienia chwili dla siebie, chowając się do łóżka, zaciemnionego pokoju czy gdziekolwiek indziej, gdzie mogę zaznać trochę spokoju i uciec od bodźców.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    q: "5. Jestem bardzo czuła / czuły na działanie kofeiny.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    q: "6. Łatwo przytłaczają mnie takie bodźce jak ostre światło, silne zapachy, szorstkie tkaniny czy syreny przejeżdżających karetek.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    q: "7. Mam bogate, skomplikowane życie wewnętrzne.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    q: "8. Głośne dźwięki sprawiają mi dyskomfort.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    q: "9. Do głębi porusza mnie sztuka i muzyka.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    q: "10. Jestem sumienna / sumienny.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 10,
+    q: "11. Łatwo mnie przestraszyć.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 11,
+    q: "12. Czuję się roztrzęsiona / roztrzęsiony, gdy mam dużo do zrobienia w krótkim czasie.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 12,
+    q: "13. Gdy ludzie nie czują się komfortowo w miejscu, w którym przebywają, zwykle wiem, jak temu zaradzić (przez zmianę oświetlenia, przestawienie foteli itp.).",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    q: "14. Irytuje mnie, gdy ludzie wymagają ode mnie zrobienia zbyt wielu rzeczy jednocześnie.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    q: "15. Bardzo dbam o to, by unikać pomyłek czy zapominania o czymś.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    q: "16. Celowo unikam filmów i programów telewizyjnych pokazujących sceny przemocy.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 16,
+    q: "17. Odczuwam nieprzyjemne pobudzenie, kiedy wokół mnie dużo się dzieje.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 17,
+    q: "18. Głód wywołuje u mnie silną reakcję, tak, że tracę koncentrację czy pogarsza mi się nastrój.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 18,
+    q: "19. Zmiany w moim życiu wytrącają mnie z równowagi.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 19,
+    q: "20. Zauważam i cenię sobie delikatne czy wysublimowane zapachy, smaki, dźwięki, dzieła sztuki.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 20,
+    q: "21. Ważnym priorytetem jest dla mnie takie ułożenie sobie życia, by unikać sytuacji denerwujących i rozstrajających.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 21,
+    q: "22. Kiedy podczas wykonywania zadania muszę z kimś rywalizować, lub jestem obserwowana / obserwowany, bardzo się denerwuję i radzę sobie o wiele gorzej niż normalnie.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+  {
+    id: 22,
+    q: "23. Mam wrażenie, że w dzieciństwie rodzice lub nauczyciele postrzegali mnie jako wrażliwą / wrażliwego czy nieśmiałą / nieśmiałego.",
+    a: [
+      { text: "tak", isCorrect: true },
+      { text: "nie", isCorrect: false },
+    ],
+  },
+];
 
 // Set start
 let start = true;
@@ -61,20 +197,20 @@ let testResult = 0;
 // Getting the question
 const question = document.getElementById("question");
 // Getting the options
-const op1 = document.getElementById('op1');
-const op2 = document.getElementById('op2');
+const op1 = document.getElementById("op1");
+const op2 = document.getElementById("op2");
 
 // Show selection for op1
 op1.addEventListener("click", () => {
-  op1.style.backgroundColor = "lightgoldenrodyellow";
+  op1.style.backgroundColor = "lightblue";
   selected = op1.value;
-})
+});
 
 // Show selection for op2
 op2.addEventListener("click", () => {
-  op2.style.backgroundColor = "lightgoldenrodyellow";
+  op2.style.backgroundColor = "lightblue";
   selected = op2.value;
-})
+});
 
 // Iterate
 function iterate(id) {
@@ -108,22 +244,22 @@ next.addEventListener("click", () => {
   if (selected === "true" && finish === false) {
     testResult = testResult + 1;
   }
-  if (id == 5) {
+  if (id == 22) {
     next.innerText = "Zakończ test";
   }
-  if (id == 6) {
+  if (id == 23) {
     finish = true;
     const result = document.getElementsByClassName("result");
     result[0].innerText = "Twój wynik testu to: " + testResult;
   }
 
-  if (id < 6) {
+  if (id < 23) {
     iterate(id);
     id++;
   }
 
-  op1.style.backgroundColor = "lightskyblue";
-  op2.style.backgroundColor = "lightskyblue";
+  op1.style.backgroundColor = "lightgrey";
+  op2.style.backgroundColor = "lightgrey";
 
-  console.log("Finalny wynik", testResult, "Id", id);
+  // console.log("Finalny wynik", testResult, "Id", id);
 });
